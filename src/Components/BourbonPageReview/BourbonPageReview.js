16e3f7d2249c605589a71f67e3a86bb68cecbc30
Golodhrim <holidayninjastaff@gmail.com>
2022-02-28 04:12:46 -0500

    clean review loop in useeffect added to review component for more consistent exp

--- src/Components/BourbonPageReview/BourbonPageReview.js
+++ src/Components/BourbonPageReview/BourbonPageReview.js
@@ -1,45 +1,55 @@
+import { useEffect, useState } from 'react';
 import { ImQuotesLeft, ImQuotesRight } from 'react-icons/im';
 import styles from './BourbonPageReview.module.scss';
 
 const BourbonPageReview = ({ review }) => {
-	const noData = `This section of the review is incomplete or has not been reviewed yet...`;
-	let { author, intro, nose, taste, finish, overall } = review;
+	const [cleanReview, setCleanReview] = useState({});
 
 	// LOOKS FOR NULL SECTIONS OF REVIEW DATA AND
 	// OVERWRITES WITH A NOT REVIEWED STRING
 
-	for (const [key] of Object.entries(review)) {
-		if (review[key] === null) {
-			review[key] = noData;
+	useEffect(() => {
+		const noData = `This section of the review is incomplete or has not been reviewed yet...`;
+		let reviewObj = {};
+		for (const [key, value] of Object.entries(review)) {
+			if (review[key] === null) {
+				reviewObj[key] = noData;
+			} else {
+				reviewObj[key] = value;
+			}
 		}
-	}
+		setCleanReview(reviewObj);
+	}, [setCleanReview, review]);
 
 	return (
 		<div className={styles.review_container}>
 			<h1>
-				<ImQuotesLeft /> {intro} <ImQuotesRight />{' '}
-				{author !==
+				<ImQuotesLeft /> {cleanReview.intro} <ImQuotesRight />{' '}
+				{cleanReview.author !==
 					'This section of the review is incomplete or has not been reviewed yet...' && (
-					<span className={styles.credit}>{`Reviewed by ${author}`}</span>
+					<span
+						className={
+							styles.credit
+						}>{`Reviewed by ${cleanReview.author}`}</span>
 				)}
 			</h1>
 			<h2 className={styles.section_text}>
 				<span className={styles.section_label}>NOSE </span>
-				{nose}
+				{cleanReview.nose}
 			</h2>
 			<h2 className={styles.section_text}>
 				<span className={styles.section_label}>TASTE </span>
-				{taste}
+				{cleanReview.taste}
 			</h2>
 			<h2 className={styles.section_text}>
 				<span className={styles.section_label}>FINISH </span>
-				{finish}
+				{cleanReview.finish}
 			</h2>
 			<h2 className={styles.section_text}>
 				<span className={`${styles.section_label} ${styles.overall}`}>
 					OVERALL{' '}
 				</span>
-				{overall}
+				{cleanReview.overall}
 			</h2>
 		</div>
 	);
