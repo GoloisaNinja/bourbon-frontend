e9d6e62696a4d191411f8e48bfd0b7825da85686
Golodhrim <holidayninjastaff@gmail.com>
2022-03-07 21:06:08 -0500

    removed unneeded imports

--- src/Components/BourbonPageUserReviewSection/UserReview.js
+++ src/Components/BourbonPageUserReviewSection/UserReview.js
@@ -1,5 +1,3 @@
-import { Link } from 'react-router-dom';
-import { HiOutlinePencil, HiOutlineTrash } from 'react-icons/hi';
 import styles from './UserReview.module.scss';
 
 const UserReview = ({ review }) => {
