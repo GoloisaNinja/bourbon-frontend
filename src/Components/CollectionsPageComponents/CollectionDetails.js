71cf75dfd79978cdd6b4a0d41e4202840d5fa8db
Golodhrim <holidayninjastaff@gmail.com>
2022-03-17 02:16:58 -0400

    collections page style updated background of no collection quicklook

--- src/Components/CollectionsPageComponents/CollectionDetails.js
+++ src/Components/CollectionsPageComponents/CollectionDetails.js
@@ -7,43 +7,47 @@ const CollectionDetails = ({ collection }) => {
 		smoothscroll.polyfill();
 		window.scroll({ top: 0, left: 0, behavior: 'smooth' });
 	};
-	return collection ? (
+	return (
 		<div className={styles.container}>
-			<div>
-				<h1>Quick Look</h1>
-				<p>{collection.bourbons.length}</p>
-			</div>
-			{collection.bourbons.length > 0 ? (
-				<div className={styles.details_container}>
-					<Link
-						to={`/collections/${collection._id}`}
-						onClick={() => scrollToTop()}>
-						Go To Collection
-					</Link>
+			{collection ? (
+				<>
 					<div>
-						<ul>
-							{collection.bourbons.map((bourbon) => (
-								<li key={bourbon._id}>
-									<Link to={`/bourbons/${bourbon.bourbon_id}`}>
-										{bourbon.title}
-									</Link>
-								</li>
-							))}
-						</ul>
+						<h1>Quick Look</h1>
+						<p>{collection.bourbons.length}</p>
 					</div>
-				</div>
+					{collection.bourbons.length > 0 ? (
+						<div className={styles.details_container}>
+							<Link
+								to={`/collections/${collection._id}`}
+								onClick={() => scrollToTop()}>
+								Go To Collection
+							</Link>
+							<div>
+								<ul>
+									{collection.bourbons.map((bourbon) => (
+										<li key={bourbon._id}>
+											<Link to={`/bourbons/${bourbon.bourbon_id}`}>
+												{bourbon.title}
+											</Link>
+										</li>
+									))}
+								</ul>
+							</div>
+						</div>
+					) : (
+						<div className={styles.empty_container}>
+							<p>Collection is empty! Go add some bourbons!</p>
+							<Link onClick={() => scrollToTop()} to={`/bourbons`}>
+								Go Explore!
+							</Link>
+						</div>
+					)}
+				</>
 			) : (
-				<div className={styles.empty_container}>
-					<p>Collection is empty! Go add some bourbons!</p>
-					<Link onClick={() => scrollToTop()} to={`/bourbons`}>
-						Go Explore!
-					</Link>
+				<div>
+					<h1>Select a collection from your list...</h1>
 				</div>
 			)}
-		</div>
-	) : (
-		<div>
-			<h1>Select a collection from your list...</h1>
 		</div>
 	);
 };
